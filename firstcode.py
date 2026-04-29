759cd49527517746b5dff708e5e8bce2d92cd5da
Shravan Rajput <shrrvvuurajput@gmail.com>
2026-04-29 11:28:28 +0530

    input

---
+++ firstcode.py
@@ -0,0 +1,6 @@
+print("Hello")
+print(23)
+
+name="shravan"
+
+print(name)
---
+++ input.py
@@ -0,0 +1,5 @@
+name=input("What is your name : ")
+name=name.capitalize()
+age=int(input("What is Your age : "))
+
+print(f"my name is {name} and i am {age} years old")
